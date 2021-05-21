7b10560d9a4d8bb8a606d14c81b0ef63306ab0a2
marcangy cange <marcangy.cange@revature.net>
2021-05-21 00:56:42 -0400

    Added Cors

--- MusicGarden.Client/Controllers/HomeController.cs
+++ MusicGarden.Client/Controllers/HomeController.cs
@@ -6,32 +6,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Medialab.Client.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Medialab.Client.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+
+    private static readonly HttpClient client = new HttpClient();
+    private IConfiguration _configuration;
+    public HomeController(IConfiguration configuration)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _configuration = configuration;
     }
+
+    public IActionResult Index()
+    {
+      var response = client.GetAsync($"{_configuration["Services:webapi"]}/Music").GetAwaiter().GetResult();
+      List<string> result = null;
+
+
+      if (response.IsSuccessStatusCode)
+      {
+        result = JsonConvert.DeserializeObject<List<String>>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+        ViewBag.Music = result;
+
+        return View("index");
+      }
+
+      return null;
+
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
